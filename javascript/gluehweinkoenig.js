7358387ef3598800c68a443f861507d713b77680
Mario Gleichmann <mario.gleichmann@orcas.de>
2022-12-02 16:31:37 +0100

    for submition chanegd

--- javascript/gluehweinkoenig.js
+++ javascript/gluehweinkoenig.js
@@ -1,28 +1,18 @@
-var userbase ="";
+var userbase = "";
 
-var form = document.querySelector('form');
-form.addEventListener('submit', function (event) {
-   event.preventDefault();
-   var data = new FormData(form);
-   submit(data);
-});
-
-function submit(data){
-
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
+function submit(data) {
+    let request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
         if (this.readyState === 4) {
-            if(this.status === 200) {
-
+            if (this.status === 200) {
                 console.log(this.responseText);
                 init();
-            } 
-            else {
+            } else {
                 failure("Leider ist ein Fehler aufgetreten. Bitte versuchen sie es erneut.");
             }
         }
     }
-    request.open("POST","../php/userlogin.php");
+    request.open("POST", "php/userlogin.php");
     request.send(data);
 }
 
@@ -33,127 +23,117 @@ function init() {
 }
 
 function fetchData(resultType, failString, requestString) {
-
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
+    let request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
         if (this.readyState === 4) {
-            if(this.status === 200) {
+            if (this.status === 200) {
                 var list = transformData(this.responseText);
                 result(list, resultType);
-            }
-            else {
+            } else {
                 result(failString, resultType);
             }
-        } 
+        }
     }
-    if(resultType == "groups") request.open("GET","../php/userhandling.php?action=fetch_grouplist");
-    else request.open("GET","../php/userhandling.php?action=fetch_userlist");
+    if (resultType == "groups") {
+        request.open("GET", "../php/userhandling.php?action=fetch_grouplist");
+    } else {
+        request.open("GET", "../php/userhandling.php?action=fetch_userlist");
+    }
     request.send();
 }
 
 function transformData(data) {
     try {
         return JSON.parse(data);
-    }
-    catch(e) {
+    } catch (e) {
         return "";
     }
 }
 
 function result(data, resultType) {
-    if(resultType == "groups") {
+    if (resultType === "groups") {
         groupResult(data);
-    }
-    else {
+    } else {
         userResult(data);
     }
 }
 
 function userResult(users) {
-
-    var userList = "";
-
-    if(!Array.isArray(users)) {
+    let userList = "";
+    if (!Array.isArray(users)) {
         userList = users;
-    }
-
-    else {
+    } else {
         userbase = users;
-        for(var x=users.length-1; x>-1; x--) {
+        for (var x = users.length - 1; x > -1; x--) {
             var row = users[x];
-            userList +=  "<p><span>" + row['user_index'] + " " + row['user_name'] + " " + row['user_group'] + "</span></p>\n";
+            userList += "<p><span>" + row['user_index'] + " " + row['user_name'] + " " + row['user_group'] + "</span></p>\n";
         }
     }
-
     $('#active_users').html(userList);
 }
 
 function groupResult(groups) {
-
-    var groupSelector =     "   <label for display_selection><b>Anzeige:</b><label>\n" + 
-                            "   <select id='year_selection' onchange='displaySwap(this.value)'>\n";
-    for(var x of groups) {
-        groupSelector +=    "       <option>" + x['group_name'] + "</option>\n";
+    let groupSelector = "   <label for display_selection><b>Anzeige:</b><label>\n" +
+        "   <select id='year_selection' onchange='displaySwap(this.value)'>\n";
+    for (let x of groups) {
+        groupSelector += "       <option>" + x['group_name'] + "</option>\n";
     }
-    groupSelector +=        "   </select>\n";
+    groupSelector += "   </select>\n";
     $('#group_selector').html(groupSelector);
 }
 
 function buttonSetupt() {
-    var url = window.location.href;
-    if(!url.includes("?admin")) {
+    let url = window.location.href;
+    if (!url.includes("?admin")) {
         $('.team_display').remove();
         $('#loginBlock').removeClass("loginBlock");
         $('#loginBlock').html($('#loginBlock').html());
-    }
-    else {
-
-        $('#team_creator').click(function(event) {
+    } else {
+        $('#team_creator').click(function (event) {
             fillTeams();
         });
     }
 }
 
 function fillTeams() {
-
     var teambase = shuffle(userbase);
     var team1 = [];
     var team2 = [];
     var team3 = [];
     var teamList = [team1, team2, team3];
-    var j=0;
+    var j = 0;
 
-    for(var u of teambase) {
+    for (var u of teambase) {
         teamList[j].push(u);
         j++;
-        if(j == 3) j=0;
+        if (j == 3) j = 0;
     }
-   
-    var teamTable = "<table id='team_table'>\n" +
-                    "   <caption><strong>Teams</strong></caption>\n" +
-                    "   <thead>\n" +
-                    "       <tr>\n";
-    for(i=0; i<teamList.length; i++) {
-        teamTable +="           <th id='team" + (i+1) + "'>Team " + (i+1) + "</th>\n";
-    }
-    teamTable +=    "       </tr>\n" +
-                    "   </thead>\n" +
-                    "   <tbody>\n";
 
-    for(var i=0; i<team1.length; i++) {
-        teamTable +="       <tr>\n";
-        for(var t=0; t<teamList.length; t++) {
+    var teamTable = "<table id='team_table'>\n" +
+        "   <caption><strong>Teams</strong></caption>\n" +
+        "   <thead>\n" +
+        "       <tr>\n";
+    for (i = 0; i < teamList.length; i++) {
+        teamTable += "           <th id='team" + (i + 1) + "'>Team " + (i + 1) + "</th>\n";
+    }
+    teamTable += "       </tr>\n" +
+        "   </thead>\n" +
+        "   <tbody>\n";
+
+    for (var i = 0; i < team1.length; i++) {
+        teamTable += "       <tr>\n";
+        for (var t = 0; t < teamList.length; t++) {
             var teamEntry = teamList[t];
-            if(teamEntry[i]) {
-                teamTable +="           <td headers='team" + (parseInt(t)+1) + "'>" + teamEntry[i]['user_name'] + " " + teamEntry[i]['user_group'] + "</td>\n";
+            if (teamEntry[i]) {
+                teamTable += "           <td headers='team" + (parseInt(t) + 1) + "'>" + teamEntry[i]['user_name'] + " " + teamEntry[i]['user_group'] + "</td>\n";
             }
         }
-        teamTable +="       </tr>\n";
+        teamTable += "       </tr>\n";
     }
 
-    teamTable +=    "   </tbody>\n" +
-                    "</table>";
-    
+    teamTable += "   </tbody>\n" +
+        "</table>";
+
     $('#team_display').html(teamTable);
 }
 
@@ -162,7 +142,7 @@ function shuffle(array) {
     var randomIndex = 0;
     var currentIndex = array.length;
 
-    while(currentIndex != 0) {
+    while (currentIndex != 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex--;
 
